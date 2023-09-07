21dc267b760cbee10042ce7c5c6d8c20dc9f911e
Hiury Lima <limahiury6@gmail.com>
2023-09-07 19:25:45 -0300

    Corrigindo nome da AtracaoViewSet.

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 
