dc2aee52af32d4215a19e1241452b2efbb0e0018
Hiury Lima <limahiury6@gmail.com>
2023-09-09 14:20:04 -0300

    Habilitando o mecanismo de Tokens para autenticação.

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
@@ -20,4 +21,5 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
