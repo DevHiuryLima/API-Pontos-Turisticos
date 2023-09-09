72e178b776499bde529e3648381ad4e70c049574
Hiury Lima <limahiury6@gmail.com>
2023-09-09 18:02:01 -0300

    Incluindo informações adicionais com SerializerMethodField e properties.

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import SerializerMethodField
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -12,6 +13,8 @@ class PontoTuristicoSerializer(ModelSerializer):
     avaliacoes = AvaliacaoSerializer(many=True)
     endereco = EnderecoSerializer()
 
+    descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
         fields = (
@@ -24,4 +27,9 @@ class PontoTuristicoSerializer(ModelSerializer):
             'comentarios',
             'avaliacoes',
             'endereco',
+            'descricao_completa',
+            'descricao_completa2',
         )
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
