4cf87f0049a32c62d294c1870c0ab224abd17b08
Hiury Lima <limahiury6@gmail.com>
2023-09-11 10:51:05 -0300

    ManyToMany relationships.

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
+
+from atracoes.models import Atracao
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -9,9 +11,7 @@ from enderecos.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    comentarios = ComentarioSerializer(many=True)
-    avaliacoes = AvaliacaoSerializer(many=True)
-    endereco = EnderecoSerializer()
+    endereco = EnderecoSerializer(read_only=True)
 
     descricao_completa = SerializerMethodField()
 
@@ -30,6 +30,20 @@ class PontoTuristicoSerializer(ModelSerializer):
             'descricao_completa',
             'descricao_completa2',
         )
+        read_only_fields = ('comentarios', 'avaliacoes')
+
+    def create_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.create_atracoes(atracoes, ponto)
+
+        return ponto
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
