8ca18d67d3693d5cfdb7567ddf5d53820c3091e3
Hiury Lima <limahiury6@gmail.com>
2023-09-19 11:26:35 -0300

    Corrigindo bug na importação das 'actions'.

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Comentario
-from actions import reprova_comentarios, aprova_comentarios
+from .actions import reprova_comentarios, aprova_comentarios
 
 
 class ComentarioAdmin(admin.ModelAdmin):
